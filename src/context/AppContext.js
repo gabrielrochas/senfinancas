b590ff4d57745e1ea26e40d84d5e6219441c8e6a
Gabriel Rocha <gabriel.rocha@join4.com.br>
2021-09-27 09:47:05 -0300

    Add first moviment as default

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -26,7 +26,7 @@ const AppReducer = (state, action) => {
 };
 
 // Initial state
-const initialState = JSON.parse(localStorage.getItem("localTransactions")) || [];
+const initialState = JSON.parse(localStorage.getItem("localTransactions")) || [{title: "Item 1", type:"Income", value: "0"}];
 
 // Create context
 export const AppContext = createContext();
